97021250e070a03d623a7a39b3342b23ff4db407
Eric <huericnan@gmail.com>
2023-02-23 13:42:09 -0500

    update

---
+++ blog-demo/resources/views/categories/index.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Categories') }}
+            </h2>
+            <a href="{{ route('categories.create') }}">
+                <x-primary-button>{{ __('New') }}</x-primary-button>
+            </a>
+        </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @foreach($categories as $category)
+            123
+            @endforeach
+        </div>
+    </div>
+</x-app-layout>
--- blog-demo/resources/views/dashboard.blade.php
+++ blog-demo/resources/views/dashboard.blade.php
@@ -9,9 +9,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-4 px-4 h-20 flex justify-between items-center">
                 <div class="text-gray-900 dark:text-gray-100">
-                    Users
+                    Categories
                 </div>
-                <button class="bg-blue-500 dark:bg-blue-500 hover:bg-blue-700 dark:hover:bg-blue-700 rounded-md text-white w-20 h-fit p-2 focus:ring">View</button>
+                <a href="{{ route('categories.index') }}"> <x-primary-button>{{ __('View') }}</x-primary-button></a>
+            </div>
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-4 px-4 h-20 flex justify-between items-center">
+                <div class="text-gray-900 dark:text-gray-100">
+                    Tags
+                </div>
+                <a href="{{ route('tags.index') }}"> <x-primary-button>{{ __('View') }}</x-primary-button></a>
+            </div>
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-4 px-4 h-20 flex justify-between items-center">
+                <div class="text-gray-900 dark:text-gray-100">
+                    Posts
+                </div>
+                <a href="{{ route('posts.index') }}"> <x-primary-button>{{ __('View') }}</x-primary-button></a>
             </div>
         </div>
     </div>
