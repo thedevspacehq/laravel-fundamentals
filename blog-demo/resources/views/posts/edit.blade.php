c9929068801185d73463e3a9b599d7d108fa1d67
Eric <huericnan@gmail.com>
2023-03-24 09:52:40 -0400

    update

--- blog-demo/resources/views/posts/edit.blade.php
+++ blog-demo/resources/views/posts/edit.blade.php
@@ -50,20 +50,9 @@
                     </form>
                     <script>
                         tinymce.init({
-                            selector: '#content',
-                            plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
-                            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-                            tinycomments_mode: 'embedded',
-                            tinycomments_author: 'Author name',
-                            mergetags_list: [{
-                                    value: 'First.Name',
-                                    title: 'First Name'
-                                },
-                                {
-                                    value: 'Email',
-                                    title: 'Email'
-                                },
-                            ]
+                            selector: 'textarea',
+                            plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+                            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
                         });
                     </script>
                 </div>
